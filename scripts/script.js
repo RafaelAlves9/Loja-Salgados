ad71d2ddc76889cdfb5c550781262f92ba621dbc
Rafael Alves <hrafael.alves9@gmail.com>
2021-11-24 17:15:04 -0300

    up

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,6 @@ const dAll = (e)=> document.querySelectorAll(e)
 // relacionando dados
 pizzaJson.map((item, index)=>{
     const pizzaItem = d('.models .pizza-item').cloneNode(true)
-    // adicionando estrutura na tela.
+    // adicionando estrutura na tela
     d('.pizza-area').append(pizzaItem)
 })
