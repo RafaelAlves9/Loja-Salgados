4b304684305cbc4c8385a8ca480b3962810c3d19
Rafael Alves <hrafael.alves9@gmail.com>
2021-11-24 18:55:07 -0300

    up

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,12 @@ const dAll = (e)=> document.querySelectorAll(e)
 // relacionando dados
 pizzaJson.map((item, index)=>{
     const pizzaItem = d('.models .pizza-item').cloneNode(true)
+
+    pizzaItem.querySelector('.pizza-item-img img').src = item.img
+    pizzaItem.querySelector('.pizza-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item-name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item-desc').innerHTML = item.description
+
     // adicionando estrutura na tela
     d('.pizza-area').append(pizzaItem)
 })
