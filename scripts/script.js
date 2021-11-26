2c186ba31ab377da74e1a50b8ac492e5415bf036
Rafael Alves <hrafael.alves9@gmail.com>
2021-11-26 16:11:45 -0300

    up

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,22 @@
 const d = (e)=> document.querySelector(e)
 const dAll = (e)=> document.querySelectorAll(e)
 
-// relacionando dados
+//relacionando dados
 salgadoJson.map((item, index)=>{
     const salgadoItem = d('.models .salgado-item').cloneNode(true)
 
-    // adicionando estruturas na tela
+    //adicionando estruturas na tela
     d('.salgado-area').append(salgadoItem)
 
-    // adicionando informações
+    //adicionando informações
     salgadoItem.querySelector('.salgado-item-img img').src = item.img
     salgadoItem.querySelector('.salgado-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`
     salgadoItem.querySelector('.salgado-item-name').innerHTML = item.name
     salgadoItem.querySelector('.salgado-item-desc').innerHTML = item.description
 
+    //aparecendo janela de escolha
+    salgadoItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault()
+        d('.salgadoWindowArea').style.display='flex'
+    })
 })
