3d903ec53e88484c22e3862118ad5434c7b2147c
Rafael Alves <hrafael.alves9@gmail.com>
2021-11-27 09:17:13 -0300

    up

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,11 @@ salgadoJson.map((item, index)=>{
     d('.salgado-area').append(salgadoItem);
 
     //adicionando informações
-    salgadoItem.setAttribute('data-key', index)
-    salgadoItem.querySelector('.salgado-item-img img').src = item.img
+    salgadoItem.setAttribute('data-key', index);
+    salgadoItem.querySelector('.salgado-item-img img').src = item.img;
     salgadoItem.querySelector('.salgado-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
-    salgadoItem.querySelector('.salgado-item-name').innerHTML = item.name
-    salgadoItem.querySelector('.salgado-item-desc').innerHTML = item.desc
+    salgadoItem.querySelector('.salgado-item-name').innerHTML = item.name;
+    salgadoItem.querySelector('.salgado-item-desc').innerHTML = item.desc;
     
 
     //aparecendo janela de escolha
@@ -21,9 +21,9 @@ salgadoJson.map((item, index)=>{
         const key = e.target.closest('.salgado-item').getAttribute('data-key');
 
         //adicionando inf window
-        d('.salgadoBig img').src = salgadoJson[key].img
-        d('.salgadoInfo h1').innerHTML = salgadoJson[key].name
-        d('.salgadoInfo-actualPrice').innerHTML = salgadoJson[key].price.toFixed(2)
-        d('.salgadoInfo-size').innerHTML = salgadoJson[key].sizes
+        d('.salgadoBig img').src = salgadoJson[key].img;
+        d('.salgadoInfo h1').innerHTML = salgadoJson[key].name;
+        d('.salgadoInfo-actualPrice').innerHTML = `R$ ${salgadoJson[key].price.toFixed(2)}`;
+        d('.salgadoInfo-size').innerHTML = salgadoJson[key].sizes;
     })
 })
